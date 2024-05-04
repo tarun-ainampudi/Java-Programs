dc3b9f92eff83141996b68bf0cc1d6eb2a6077be
tarun-ainampudi <tarunainampudi@gmail.com>
2024-05-05 01:11:53 +0530

    Change in P7

--- Practice/FileHandling.java
+++ Practice/FileHandling.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class P7 {
+public class FileHandling {
     public static void main(String[] args) throws FileNotFoundException,NumberFormatException,ArithmeticException{
         
        try{
